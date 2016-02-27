ce623063421c0ca258c2e5a749a1e27398530bcf
Dennis Chirwa <dchirwa@UKM0415241F1.home>
2016-02-27 15:40:23 +0000

    added test to title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ryde' });
+  res.render('index', { title: 'Test Ryde' });
 });
 
 module.exports = router;
