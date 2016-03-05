22aafee8ea74ff4bcb8279686bba610209ae3532
Paul Nicholson <Paul@Pauls-Mac-mini.home>
2016-03-05 20:42:25 +0000

    Began implmentation of auth

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var routes = require('./routes/index');
+
 var app = express();
+
+var mongoConnection = 'mongodb://RydeMongo01:c0pperKettle@ds062178.mlab.com:62178/RYDEMongo';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +27,14 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(''));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -28,6 +45,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
+mongoose.connect(mongoConnection);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
 var router = express.Router();
 
 /* GET home page. */
@@ -11,12 +13,44 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
+/* POST login page. */
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    
+    
+    res.redirect('/console');
+});
+
+/* GET logout page. */
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET platform page. */
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+/* POST register page. */
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+          res.redirect('/');
+        });
+    });
+  });
+
+
+/* GET console page. */
 router.get('/console', function(req, res, next) {
   res.render('console', { title: 'Console' });
 });
